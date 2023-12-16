0b0f47981d004b4ddd0f7a468490dfa582c48990
otakakou <kou_otaka@yahoo.co.jp>
2023-12-16 12:30:12 +0900

    first commit2

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    
+    protected $fillable = [
+    'title',
+    'body',
+    'user_id'
+    ];
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+}
+
+
